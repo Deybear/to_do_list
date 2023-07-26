3077c7dc0d6bd7f026bfd51775fecd605dc58b1f
Deybear <drodriguezr@fwdcostarica.com>
2023-07-26 15:44:35 -0600

    Update

--- js/to_do.js
+++ js/to_do.js
@@ -3,13 +3,16 @@
 
 // Calling task container . . .
 var taskContainer = document.getElementById("task_container");
-//var taskContainer = Array.from(document.getElementById("task_container"));
 
 // Calling task input . . .
 var addInput = document.getElementById("add_input");
 
 // Calling add button . . .
 var addBtn = document.getElementById("add_btn");
+
+// Calling add button . . .
+var counter = document.getElementById("counter");
+var doneTask = 0;
 
 /* - - - - - - - - - - || CLICK EVENT || - - - - - - - - - - */
 function addTask()
@@ -18,7 +21,7 @@ function addTask()
     if(addInput.value === '')
     {
         // Calling an alert . . .
-        alert("CHINGUE A SU MADRE!!");
+        alert("Ingrese un texto");
     }
     else
     {
@@ -28,7 +31,7 @@ function addTask()
         const createDiv = document.createElement("div");
         
         // Adding (i) to (div) . . .
-        createDiv.innerHTML = '<i id="delete_btn" class="material-icons">remove_circle</i>';
+        createDiv.innerHTML = '<i class="material-icons delete_btn">remove_circle</i>';
 
         /* - - - - - - - - - - || SPAN ELEMENTS || - - - - - - - - - - */
 
@@ -39,7 +42,7 @@ function addTask()
         createDiv.appendChild(createSpan);
 
         // Adding (i) to (span) . . .
-        createSpan.innerHTML = '<i id="task_checker" class="material-icons">radio_button_unchecked</i>';
+        createSpan.innerHTML = '<i class="material-icons task_checker" value="0">radio_button_unchecked</i>';
 
         /* - - - - - - - - - - || P ELEMENTS || - - - - - - - - - - */
 
@@ -56,18 +59,103 @@ function addTask()
 
         // Adding task to container . . .
         taskContainer.appendChild(createDiv);
-        //taskContainer.push(createDiv);
 
         // Adding syle to task . . .
         createDiv.classList.add("task");
+
+        checkTask(createDiv);
+        deleteTask();
     }
 
     addInput.value = "";
 }
 
+/* - - - - - - - - - - || COUNTER EVENT || - - - - - - - - - - */
+function increaseCounter()
+{
+    // Increase counter . . .
+    doneTask++;
+
+    // Add counter to HTML . . .
+    counter.textContent = doneTask;
+}
+
+function decreaseCounter()
+{
+    // Decrease counter . . .
+    doneTask--;
+
+    //Fix zero issue . . .
+    if(doneTask <= 0)
+    {
+        doneTask = 0;
+    }
+
+    // Add counter to HTML . . .
+    counter.textContent = doneTask;
+}
+
+/* - - - - - - - - - - || CHECK EVENT || - - - - - - - - - - */
+function checkTask(createDiv)
+{
+    // Get last element from container . . .
+    const getIconCheck = taskContainer.lastElementChild;
+
+    // Get icon by class . . .
+    const checkBtn = getIconCheck.querySelector(".task_checker");
+
+    // Check function . . .
+    checkBtn.addEventListener("click", function()
+    {
+        //Change value to int . . .
+        var value = parseInt(checkBtn.value);
+
+        //Click checker . . .
+        if (value == 0)
+        {
+            increaseCounter();
+            createDiv.classList.remove("task");
+            createDiv.classList.add("checked_task");
+            checkBtn.textContent = "radio_button_checked";
+
+            //Change value . . .
+            checkBtn.value = 1;
+        } 
+        else
+        {
+            decreaseCounter();
+            createDiv.classList.remove("checked_task");
+            createDiv.classList.add("task");
+            checkBtn.textContent = "radio_button_unchecked";
+
+            //Change value . . .
+            checkBtn.value = 0;
+        }
+        
+    });
+}
+
+/* - - - - - - - - - - || DELETE EVENT || - - - - - - - - - - */
+function deleteTask()
+{
+    // Get last element from container . . .
+    const newTaskDiv = taskContainer.lastElementChild;
+
+    // Get icon by class . . .
+    const deleteBtn = newTaskDiv.querySelector(".delete_btn");
+
+    // Delete function . . .
+    deleteBtn.addEventListener("click", function(event)
+    {
+        const taskDiv = event.target.parentNode;
+        taskDiv.remove();
+    });
+}
+
 /* - - - - - - - - - - || ENTER EVENT || - - - - - - - - - - */
 addInput.addEventListener("keyup", (event) => 
 {
+    // If [ENTER] is pressed so . . .
     if (event.key === "Enter") 
     {
         // Print message on console . . .
